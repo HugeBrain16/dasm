10e72aecef8513cd5e8fc7014e7e8636d477a629
HugeBrain16 <joshtuck373@gmail.com>
2021-10-03 19:37:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ md = {
     "url": "https://github.com/HugeBrain16/dasm",
     "license": "MIT",
     "description": "WAYTOODANK assembly discord bot",
-    "requires": ["cmdtools-py", "discord.py"],
+    "install_requires": ["cmdtools-py", "discord.py"],
     "packages": ["dasm", ],
     "long_description": open("README.md", "r", encoding="UTF8").read(),
     "long_description_content_type": "text/markdown",
